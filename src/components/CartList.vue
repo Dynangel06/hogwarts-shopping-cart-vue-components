4fd949c53f79f272341bada4821dd3613cc9c4a2
Dynangel2006 <angelranget@icloud.com>
2026-03-12 18:28:36 -0500

    hogwarts-shopping-cart-vue-components

---
+++ src/components/CartList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <cartListItem v-for="cartItem in cartItems"
+        :key="cartItem.id"
+        :item="cartItem"
+        @item-remove="$emit('item-remove', $event)"
+        @quantity-update="$emit('quantity-update', $event)"
+        ></cartListItem>
+    </div>
+</template>
+
+<script setup>
+import cartListItem from './cartListItem.vue';
+defineProps(['cartItems'])
+defineEmits(['item-remove', 'quantity-update'])
+</script>
+
+<style scoped></style>
